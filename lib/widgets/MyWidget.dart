8acef8605e1607bfe49a3b9977beda6d0c26b1f1
Harshang0311 <24cp432@bvmengineering.ac.in>
2026-05-18 15:54:36 +0530

    splash final done

--- lib/widgets/MyWidget.dart
+++ lib/widgets/MyWidget.dart
@@ -15,7 +15,7 @@ class _MyWidgetState extends State<MyWidget> {
       width: double.infinity,
       height: double.infinity,
       decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [Color.fromARGB(255, 82, 154, 226),Color.fromARGB(198, 139, 139, 139)],
+        gradient: LinearGradient(colors: [Color.fromARGB(255, 255, 255, 255),Color.fromARGB(197, 79, 174, 182)],
         begin: Alignment.topCenter,
         end:Alignment.bottomCenter)
       ),
@@ -24,16 +24,16 @@ class _MyWidgetState extends State<MyWidget> {
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
-            children: [Text("Version : 1.0",style: TextStyle(fontSize: 20),)],
+            children: [Padding(padding: EdgeInsets.all(10),child: Text("V : 1.0"),)],
           ),
-          SizedBox(height: 50,),
+          SizedBox(height: 150,),
           Image.asset('images/logo.png',width: 200,height: 200,),
-          Text("Library Management",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 40),),
+          Text("Digital Library",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 40),),
           SizedBox(height: 50,),
           CircularProgressIndicator(color: Colors.white,strokeWidth: 5,),
           SizedBox(height: 5,),
           Text("Loading ...",style: TextStyle(fontSize: 20,color: Colors.black),),
-          SizedBox(height: 150,),
+          Spacer(),
           Text("© 2026 Library Management",style: TextStyle(color: Colors.black),)
         ],
       ),
